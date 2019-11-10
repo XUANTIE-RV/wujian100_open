490a5a80561706cd65ad7d65999eebb727a5fe29
ExplorerLog <850345193@qq.com>
2019-11-10 13:30:06 +0800

    Update Srec2vmem.py

--- tools/Srec2vmem.py
+++ tools/Srec2vmem.py
@@ -116,12 +116,12 @@ def hex2vmem(o_argv_info):
 
         addr_chars = addr_bits / char_bit_num
 
-        line_addr_idx = record_type_chars + line_byte_chars
-        line_addr_end = line_addr_idx + addr_chars
+        line_addr_idx = int(record_type_chars + line_byte_chars)
+        line_addr_end = int(line_addr_idx + addr_chars)
 
         data_chars = (line_bytes * byte_char_num) - addr_chars - check_sum_chars
-        line_data_idx = line_addr_end
-        line_data_end = line_data_idx + data_chars
+        line_data_idx = int(line_addr_end)
+        line_data_end = int(line_data_idx + data_chars)
 
         #print("addr_chars:%d" %addr_chars)
         #print("line_addr_idx:%d" %line_addr_idx)
@@ -144,7 +144,7 @@ def hex2vmem(o_argv_info):
             
         line_addr = line_addr - base_addr
 
-        cur_mem_addr = line_addr / 4
+        cur_mem_addr = int(line_addr / 4)
         
         if prev_mem_addr == -1:
             prev_mem_addr = cur_mem_addr
